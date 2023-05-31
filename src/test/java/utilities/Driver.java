00586b293391e28e4c68e4709e6f7b0b7d736435
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-31 14:54:23 +0300

    31.05.2023

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,75 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class Driver {
+    /*
+    POM'de Driver için TestBase Classına extend etmek yerine  Driver classından
+    static methodlar kullanarak driver oluşturulup, ilgili ayarların yapılması ve en sonda
+    driverın kapatılması tercih edilmiştir.
+
+    POM'de Driver classındaki getDriver methodunun obje oluşturularak kullanılmasını
+    engellemek için  Singleton pattern kullanımı benimsenmiştir.
+
+    Singleton pattern : Tekli kullanım, bir classın farklı classlardan obje oluşturularak kullanımını
+    engellemek için kullanılır.
+
+    Bunu sağlamak için yapmamız gereken şey oldukça basit obje oluşturmak için kullanılan
+    constructoru private yaptığınızda baska classlarda Driver classından obje oluşturulması
+    mümkün OLAMAZ.
+     */
+
+    private Driver(){
+
+    }
+
+    static WebDriver driver;
+
+    public static WebDriver getDriver() {
+
+        if (driver == null) {
+            switch (ConfigReader.getProperty("browser")){
+                case "chrome" :
+                    WebDriverManager.chromedriver().setup();
+                    driver=new ChromeDriver();
+                    break;
+                case "edge" :
+                    WebDriverManager.edgedriver().setup();
+                    driver=new EdgeDriver();
+                    break;
+                case "firefox" :
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new FirefoxDriver();
+                    break;
+                case "headless-chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver=new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver=new ChromeDriver();
+
+            }
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        }
+        return driver;
+    }
+
+        public static void closeDriver () {
+            if (driver != null) {
+                driver.close();
+                driver = null;
+            }
+        }
+
+
+}
---
+++ target/test-classes/features/amazonSearch.feature
@@ -0,0 +1,20 @@
+Feature: Amazon Search
+
+  Scenario: TC01 kullanici amazonda Nutella aratir
+
+    Given kullanici amazon anasayfasinda
+    Then kullanici Nutella icin arama yapar
+    And sonuclarin Nutella icerdigini test eder
+    And sayfayi kapatir
+
+  Scenario: TC02 kullanici amazonda Java aratir
+    Given kullanici amazon anasayfasinda
+    Then kullanici Java icin arama yapar
+    And sonuclarin Java icerdigini test eder
+    And sayfayi kapatir
+
+  Scenario: TC03 kullanici amazonda xiaomi aratir
+    Given kullanici amazon anasayfasinda
+    Then kullanici xiaomi icin arama yapar
+    And sonuclarin xiaomi icerdigini test eder
+    And sayfayi kapatir
